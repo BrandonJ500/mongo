8141fa5c1f78a095528bd938c98dfb25e90c0658
Brandon Joseph <Brandonjoseph805@gmail.com>
2023-04-09 13:11:00 -0700

    update validator test

--- product.js
+++ product.js
@@ -39,15 +39,24 @@ const productSchema = new mongoose.Schema({
 
 const Product = mongoose.model("Product", productSchema);
 
-const bike = new Product({ name: "Mountain Bike", price: 19.50, color:"red", catagories: ["cycling","Safety"], qty: {online:5,inStore:10}});
+// const bike = new Product({ name: "Mountain Bike", price: 19.50, color:"red", catagories: ["cycling","Safety"], qty: {online:5,inStore:10}});
 
-bike.save()
-    .then((data) => {
-        console.log("It worked");
-        console.log(data);
-    })
-    .catch((err) => {
-        console.log("Failed")
-        console.log(err)
-    })
+// bike.save()
+//     .then((data) => {
+//         console.log("It worked");
+//         console.log(data);
+//     })
+//     .catch((err) => {
+//         console.log("Failed")
+//         console.log(err)
+//     })
 
+Product.findOneAndUpdate({name: "Tire Pump"}, {price: -19.99}, {new: true, runValidators: true})
+.then(data=>{
+    console.log("it worked");
+    console.log(data);
+})
+.catch(err=> {
+    console.log("didn't work");
+    console.log(err)
+})
